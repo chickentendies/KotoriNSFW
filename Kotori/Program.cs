f0947884a82afdbf9f194c37650b0d1d3c1bbc0a
Julian van de Groep <me@flash.moe>
2018-08-03 17:59:20 +0200

    Add project files.

---
+++ Kotori/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kotori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
